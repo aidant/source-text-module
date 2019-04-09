dfb0f013add965445befd1ed5ae66cde31292730
aidant <aidan@grumpysailor.com>
2019-04-09 20:46:42 +1200

    Use deno code in test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { resolver } from './plugins/resolver'
 import { fileLoader } from './plugins/file-loader'
 import { typescriptTranspiler } from './plugins/typescript-transpiler'
 import { TranspileOptions, ScriptTarget } from 'typescript'
+import { httpLoader } from './plugins/http-loader'
 
 const tsOptions: TranspileOptions = {
   compilerOptions: {
@@ -10,13 +11,22 @@ const tsOptions: TranspileOptions = {
   }
 }
 
+const scope = {
+  console,
+  Deno: { noColor: false }
+}
+
 const app = application({
-  scope: { console },
+  scope,
   resolver: resolver({ extensions: ['.ts'] }),
   loaders: [
     {
       test: /file:/,
       handler: fileLoader
+    },
+    {
+      test: /https:/,
+      handler: httpLoader
     }
   ],
   transpilers: [
--- test/log.ts
+++ test/log.ts
@@ -1,1 +1,3 @@
-export const log = (...args: string[]) => console.log(...args)
+import * as colours from 'https://deno.land/std@v0.3.2/colors/mod.ts'
+
+export const log = (...args: string[]) => console.log(colours.cyan('Logger:'), ...args)
