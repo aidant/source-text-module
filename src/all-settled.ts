2d9fb23e82e046ccbc9183a31e1c07a9d9be6c4f
aidant <aidan.temple1@gmail.com>
2019-05-05 00:11:30 +1000

    Remove explicit return

--- src/all-settled.ts
+++ src/all-settled.ts
@@ -1,9 +1,7 @@
-export const settled = <T>(
-  promise: Promise<T>
-): { status: 'fulfilled'; value: T } | { status: 'rejected'; reason: any } =>
+export const settled = <T>(promise: Promise<T>) =>
   promise.then(
     value => ({ status: 'fulfilled', value }),
     reason => ({ status: 'rejected', reason })
-  ) as any
+  )
 
 export const allSettled = <T>(promises: Promise<T>[]) => Promise.all(promises.map(settled))
