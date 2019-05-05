002adeb7557ce42575362ac348cb1ea6daba9316
aidant <aidan.temple1@gmail.com>
2019-05-05 23:13:31 +1000

    Remove unused module all-settled

--- src/all-settled.ts
+++
@@ -1,7 +0,0 @@
-export const settled = <T>(promise: Promise<T>) =>
-  promise.then(
-    value => ({ status: 'fulfilled', value }),
-    reason => ({ status: 'rejected', reason })
-  )
-
-export const allSettled = <T>(promises: Promise<T>[]) => Promise.all(promises.map(settled))
